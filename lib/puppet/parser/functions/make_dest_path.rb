02f74977f582e04123719460ba131439e560e420
Jetroid <holtjet@gmail.com>
2015-08-18 12:28:18 +0100

    Added function to convert source filepath and dest dirpath to dest filepath.

---
+++ lib/puppet/parser/functions/make_dest_path.rb
@@ -0,0 +1,12 @@
+module Puppet::Parser::Functions
+  newfunction(:make_dest_path) do |args|
+    source_filepath = args[0]
+    dest_dirpath = args[1]
+    # Split to get filename.
+    filename = sourcefilepath.split("/").last
+    # If last character of destination_dirpath not a slash, then add one.
+    dest_dirpath = dest_dirpath[-1,1] == "/" ? dest_dirpath : dest_dirpath + "/"
+    #Append filename.
+    return dest_dirpath + filename
+  end
+end

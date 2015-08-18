6a6ca136fba8c74aa412e84181cf6151748c3ca5
Jetroid <holtjet@gmail.com>
2015-08-18 17:19:59 +0100

    Minor formatting issue.

--- manifests/params.pp
+++ manifests/params.pp
@@ -2,7 +2,7 @@
 class branding::params {
   $ensure                           = 'present'
   $desktop_filepath_source          = '/usr/share/desktops/warty-final-ubuntu.png'
-  $desktop_dirpath_dest            = '/usr/share/desktops/'
+  $desktop_dirpath_dest             = '/usr/share/desktops/'
   $login_filepath_source            = '/usr/share/desktops/warty-final-ubuntu.png'
-  $login_dirpath_dest              = '/usr/share/desktops/'
+  $login_dirpath_dest               = '/usr/share/desktops/'
 }

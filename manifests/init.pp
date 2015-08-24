05b15af05f42f4b33e81bbd4a58ac9b4571db8c3
Jetroid <holtjet@gmail.com>
2015-08-24 16:02:12 +0100

    Correct variable to be boolean
    Simplify conditional in template

--- manifests/init.pp
+++ manifests/init.pp
@@ -15,9 +15,9 @@ class branding(
 ) inherits branding::params {
 
   validate_re($ensure, '^(present|absent)$',"${ensure} is not allowed for the 'ensure' parameter. Allowed values are 'present' and 'absent'.")
-  validate_re($blank_screen,'^(true|false)$',"${blank_screen} is not allowed for the 'blank_screen' parameter. Allowed values are 'true' and 'false'.")
-  validate_re($draw_user_backgrounds,'^(true|false)$',"${draw_user_backgrounds} is not allowed for the 'draw_user_backgrounds' parameter. Allowed values are 'true' and 'false'.") 
-  validate_re($draw_grid,'^(true|false)$',"${draw_grid} is not allowed for the 'draw_grid' parameter. Allowed values are 'true' and 'false'.")
+  validate_bool($blank_screen)
+  validate_bool($draw_user_backgrounds) 
+  validate_bool($draw_grid)
   validate_string($desktop_filepath_source,$desktop_filepath_dest,$login_filepath_source,$login_filepath_dest)
 
   anchor { 'branding::begin': } ->
